f7fc74be5199a787c55c00385c8f80f239edb60c
shadab36 <team.qanalyst@gmail.com>
2018-10-23 10:12:47 +0000

    test

--- src/main/java/webApp/CommonData.java
+++ src/main/java/webApp/CommonData.java
@@ -5,18 +5,18 @@ public class CommonData {
 	
 	//Chrome driver
 	public static String Chrome_Name="webdriver.chrome.driver";
-	public static String Chrome_Path="F:\\Driver of All\\chromedriver.exe";
+	public static String Chrome_Path="chromedriver.exe";
 	
 	// Firefox driver
 	public static String Firefox_Name="webdriver.gecko.driver";
-	public static String Firefox_Path="F:\\Driver of All\\geckodriver.exe";
+	public static String Firefox_Path="geckodriver.exe";
 	
 	//Edge driver
 	public static String Edge_Name="webdriver.edge.driver";
-	public static String Edge_Path="F:\\Driver of All\\MicrosoftWebDriver.exe";
+	public static String Edge_Path="MicrosoftWebDriver.exe";
 	
 	//IE driver
 	public static String IE_Name="webdriver.ie.driver";
-	public static String IE_Path="F:\\Driver of All\\IEDriverServer.exe";
+	public static String IE_Path="IEDriverServer.exe";
 	
 }
